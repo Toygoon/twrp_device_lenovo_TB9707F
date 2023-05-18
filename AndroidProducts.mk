e80e35dc46f951991efdafb90af519b6f3e9eaf6
Toygoon <toygoon@gmail.com>
2023-05-18 15:56:56 +0900

    TB9707F: initial tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TB-9707F.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TB-9707F-user \
+    omni_TB-9707F-userdebug \
+    omni_TB-9707F-eng
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# twrp_device_lenovo_TB9707F
+# Android device tree for Lenovo Lenovo TB-9707F (TB-9707F)
+
+```
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
